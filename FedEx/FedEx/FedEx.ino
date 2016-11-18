6cdea2dc26b6e21289e5aec8d68ec349c661583d
kageshimo <kageshimo@googlemail.com>
2016-11-18 14:04:26 +0100

    test

--- FedEx/FedEx/FedEx.ino
+++ FedEx/FedEx/FedEx.ino
@@ -60,6 +60,8 @@ int fuzzyCounter = 0;
 
 int turnCounter = 0;
 
+boolean automatic = true;
+
 
 void ov1812() {
   int eighth = 125;
@@ -94,17 +96,31 @@ void loop() {
 
   startButton();
 
-  while (true) {
+  while (automatic) {
 
     if (!crashed) {
       followTheLine();
     } else {
       evade();
     }
-    
 	  doNotCrash();
+	  checkButton();
+  }
+  while(true){
+	ir_irMovement();
+	ir_doNotCrash();
   }
 
+}
+
+void checkButton() {
+	if (analogRead(7) < 100) {
+		automatic = false;
+		led.setColorAt(0, 0, 255, 0);
+		led.setColorAt(1, 0, 255, 0);
+		led.show();
+		buzzer.tone(1000, 200);
+	}
 }
 
 
@@ -345,4 +361,142 @@ void evade() {
       }
     }
   }
+}
+
+
+
+void ir_irMovement()
+{
+	if (ir.decode())
+	{
+		uint32_t value = ir.value;
+		value = value >> 16 & 0xff;
+		switch (value)
+		{
+
+		case IR_BUTTON_LEFT:
+			ir_left();
+			break;
+		case IR_BUTTON_RIGHT:
+			ir_right();
+			break;
+		case IR_BUTTON_UP:
+			ir_forward();
+			break;
+		case IR_BUTTON_DOWN:
+			ir_backward();
+			break;
+		case IR_BUTTON_SETTING:
+			ir_stop();
+			break;
+		case IR_BUTTON_0:
+			runSpeed = 255 * 1 / 10;
+			break;
+		case IR_BUTTON_1:
+			runSpeed = 255 * 2 / 10;
+			break;
+		case IR_BUTTON_2:
+			runSpeed = 255 * 3 / 10;
+			break;
+		case IR_BUTTON_3:
+			runSpeed = 255 * 4 / 10;
+			break;
+		case IR_BUTTON_4:
+			runSpeed = 255 * 5 / 10;
+			break;
+		case IR_BUTTON_5:
+			runSpeed = 255 * 6 / 10;
+			break;
+		case IR_BUTTON_6:
+			runSpeed = 255 * 7 / 10;
+			break;
+		case IR_BUTTON_7:
+			runSpeed = 255 * 8 / 10;
+			break;
+		case IR_BUTTON_8:
+			runSpeed = 255 * 9 / 10;
+			break;
+		case IR_BUTTON_9:
+			runSpeed = 255;
+			break;
+		default:break;
+		}
+	}
+}
+
+void ir_forward()
+{
+	motor1.run(-runSpeed);
+	motor2.run(runSpeed);
+	direction = M_FORWARD;
+}
+
+void ir_backward()
+{
+	motor1.run(runSpeed);
+	motor2.run(-runSpeed);
+	direction = M_BACKWARD;
+}
+
+void ir_stop()
+{
+	motor1.run(0);
+	motor2.run(0);
+	direction = M_STOP;
+}
+
+void ir_right()
+{
+	if (direction == M_FORWARD)
+	{
+		motor1.run(-runSpeed);
+		motor2.run(int(runSpeed / cornerQuotient));
+		return;
+	}
+	if (direction == M_BACKWARD)
+	{
+		motor1.run(runSpeed);
+		motor2.run(-int(runSpeed / cornerQuotient));
+		return;
+	}
+	if (direction == M_STOP)
+	{
+		motor1.run(-runSpeed / 2);
+		motor2.run(-runSpeed / 2);
+		return;
+	}
+}
+
+void ir_left()
+{
+	if (direction == M_FORWARD)
+	{
+		motor1.run(-int(runSpeed / cornerQuotient));
+		motor2.run(runSpeed);
+		return;
+	}
+
+	if (direction == M_BACKWARD)
+	{
+		motor1.run(int(runSpeed / cornerQuotient));
+		motor2.run(-runSpeed);
+		return;
+	}
+	if (direction == M_STOP)
+	{
+		motor1.run(runSpeed / 2);
+		motor2.run(runSpeed / 2);
+		return;
+	}
+}
+
+void ir_doNotCrash()
+{
+	if (direction == M_FORWARD && ultrasonic.distanceCm() < 5)
+	{
+		stop();
+		buzzer.tone(1000, 200);	//Buzzer sounds 1200Hz for 1000ms
+		delay(100);
+		buzzer.tone(1000, 200);	//Buzzer sounds 1200Hz for 1000ms
+	}
 }
