4963ec376f9c9a8624637a01fe59735ddcb9b3d8
Tobias Dittrich <dittrich@wave-computer.de>
2016-11-18 08:32:13 +0100

    format

--- FedEx/FedEx/FedEx.ino
+++ FedEx/FedEx/FedEx.ino
@@ -1,7 +1,7 @@
 /*
- Name:		FedEx.ino
- Created:	17.11.2016 16:45:56
- Author:	kages
+  Name:		FedEx.ino
+  Created:	17.11.2016 16:45:56
+  Author:	kages
 */
 
 // the setup function runs once when you press reset or power the board
@@ -38,12 +38,12 @@ int minFallbackSpeed = 190;
 int minRunSpeed = 100;
 bool crashed = false;
 
-#define NOTE_c 261 
+#define NOTE_c 261
 #define NOTE_d 294
 #define NOTE_e 329
-#define NOTE_f 349 
-#define NOTE_g 392 
-#define NOTE_a 440 
+#define NOTE_f 349
+#define NOTE_g 392
+#define NOTE_a 440
 #define NOTE_b 493
 #define NOTE_C 523
 #define NOTE_D 587
@@ -58,30 +58,30 @@ int fuzzyCounter = 0;
 
 
 void ov1812() {
-	int eighth = 125;
+  int eighth = 125;
 
-	buzzer.tone(NOTE_R, eighth);
-	buzzer.tone(NOTE_g, eighth);
-	buzzer.tone(NOTE_C, eighth);
-	buzzer.tone(NOTE_D, eighth);
-	buzzer.tone(NOTE_E, eighth);
-	buzzer.tone(NOTE_D, eighth);
-	buzzer.tone(NOTE_C, eighth);
-	buzzer.tone(NOTE_D, eighth);
-	buzzer.tone(NOTE_E, 2 * eighth);
-	buzzer.tone(NOTE_C, 2 * eighth);
-	buzzer.tone(NOTE_C, 4 * eighth);
+  buzzer.tone(NOTE_R, eighth);
+  buzzer.tone(NOTE_g, eighth);
+  buzzer.tone(NOTE_C, eighth);
+  buzzer.tone(NOTE_D, eighth);
+  buzzer.tone(NOTE_E, eighth);
+  buzzer.tone(NOTE_D, eighth);
+  buzzer.tone(NOTE_C, eighth);
+  buzzer.tone(NOTE_D, eighth);
+  buzzer.tone(NOTE_E, 2 * eighth);
+  buzzer.tone(NOTE_C, 2 * eighth);
+  buzzer.tone(NOTE_C, 4 * eighth);
 
 }
 
 void setup() {
-	led.setpin(13);
-	Serial.begin(9600);
-	pinMode(7, INPUT); //Define button pin as input
-	led.setColorAt(0, 255, 0, 0);
-	led.setColorAt(1, 255, 0, 0);
-	led.show();
-	ov1812();
+  led.setpin(13);
+  Serial.begin(9600);
+  pinMode(7, INPUT); //Define button pin as input
+  led.setColorAt(0, 255, 0, 0);
+  led.setColorAt(1, 255, 0, 0);
+  led.show();
+  ov1812();
 }
 
 
@@ -91,11 +91,8 @@ void loop() {
   startButton();
 
   while (true) {
-    //ledExample();
-    //irMovement();
-	
     if (!crashed) {
-		followTheLine();
+      followTheLine();
     } else {
       evade();
     }
@@ -107,95 +104,95 @@ void loop() {
 
 void irMovement()
 {
-	if (ir.decode())
-	{
-		uint32_t value = ir.value;
-		value = value >> 16 & 0xff;
-		switch (value)
-		{
+  if (ir.decode())
+  {
+    uint32_t value = ir.value;
+    value = value >> 16 & 0xff;
+    switch (value)
+    {
 
-		case IR_BUTTON_LEFT:
-			left();
-			break;
-		case IR_BUTTON_RIGHT:
-			right();
-			break;
-		case IR_BUTTON_UP:
-			forward();
-			break;
-		case IR_BUTTON_DOWN:
-			backward();
-			break;
-		case IR_BUTTON_SETTING:
-			stop();
-			break;
-		case IR_BUTTON_0:
-			runSpeed = 255 * 1 / 10;
-			break;
-		case IR_BUTTON_1:
-			runSpeed = 255 * 2 / 10;
-			break;
-		case IR_BUTTON_2:
-			runSpeed = 255 * 3 / 10;
-			break;
-		case IR_BUTTON_3:
-			runSpeed = 255 * 4 / 10;
-			break;
-		case IR_BUTTON_4:
-			runSpeed = 255 * 5 / 10;
-			break;
-		case IR_BUTTON_5:
-			runSpeed = 255 * 6 / 10;
-			break;
-		case IR_BUTTON_6:
-			runSpeed = 255 * 7 / 10;
-			break;
-		case IR_BUTTON_7:
-			runSpeed = 255 * 8 / 10;
-			break;
-		case IR_BUTTON_8:
-			runSpeed = 255 * 9 / 10;
-			break;
-		case IR_BUTTON_9:
-			runSpeed = 255;
-			break;
-		default:break;
-		}
-	}
+      case IR_BUTTON_LEFT:
+        left();
+        break;
+      case IR_BUTTON_RIGHT:
+        right();
+        break;
+      case IR_BUTTON_UP:
+        forward();
+        break;
+      case IR_BUTTON_DOWN:
+        backward();
+        break;
+      case IR_BUTTON_SETTING:
+        stop();
+        break;
+      case IR_BUTTON_0:
+        runSpeed = 255 * 1 / 10;
+        break;
+      case IR_BUTTON_1:
+        runSpeed = 255 * 2 / 10;
+        break;
+      case IR_BUTTON_2:
+        runSpeed = 255 * 3 / 10;
+        break;
+      case IR_BUTTON_3:
+        runSpeed = 255 * 4 / 10;
+        break;
+      case IR_BUTTON_4:
+        runSpeed = 255 * 5 / 10;
+        break;
+      case IR_BUTTON_5:
+        runSpeed = 255 * 6 / 10;
+        break;
+      case IR_BUTTON_6:
+        runSpeed = 255 * 7 / 10;
+        break;
+      case IR_BUTTON_7:
+        runSpeed = 255 * 8 / 10;
+        break;
+      case IR_BUTTON_8:
+        runSpeed = 255 * 9 / 10;
+        break;
+      case IR_BUTTON_9:
+        runSpeed = 255;
+        break;
+      default: break;
+    }
+  }
 }
 
 void forward()
 {
-	motor1.run(-runSpeed);
-	motor2.run(runSpeed);
-	direction = M_FORWARD;
+  motor1.run(-runSpeed);
+  motor2.run(runSpeed);
+  direction = M_FORWARD;
 }
 
 void backward()
 {
-	motor1.run(minRunSpeed);
-	motor2.run(-minRunSpeed);
-	direction = M_BACKWARD;
-	runSpeed = minRunSpeed;
+  motor1.run(minRunSpeed);
+  motor2.run(-minRunSpeed);
+  direction = M_BACKWARD;
+  runSpeed = minRunSpeed;
 }
 
 void stop()
 {
-	motor1.run(0);
-	motor2.run(0);
-	direction = M_STOP;
+  motor1.run(0);
+  motor2.run(0);
+  direction = M_STOP;
 }
 
 void right()
 {
-		motor1.run(-runSpeed);
-		motor2.run(-int(runSpeed / cornerQuotient));
+  motor1.run(-runSpeed);
+  motor2.run(-int(runSpeed / cornerQuotient));
 }
 
 void left()
 {
-		motor1.run(int(runSpeed / cornerQuotient));
-		motor2.run(runSpeed);
+  motor1.run(int(runSpeed / cornerQuotient));
+  motor2.run(runSpeed);
 }
 
 void doNotCrash()
@@ -207,72 +204,71 @@ void doNotCrash()
 }
 
 void startButton() {
+  while (analogRead(7) > 100) {
+    delay(50);
+  }
 
-	while (analogRead(7) > 100) {
-		delay(50);
-	}
+  for (int i = 0; i < 5; i++) {
+    led.setColorAt(0, 0, 255, 0);
+    led.setColorAt(1, 0, 255, 0);
+    led.show();
+    delay(800);
+    led.setColorAt(0, 0, 0, 0);
+    led.setColorAt(1, 0, 0, 0);
+    led.show();
+    buzzer.tone(1000, 200);
+  }
 
-	for (int i = 0; i< 5; i++){
-		led.setColorAt(0, 0, 255, 0);
-		led.setColorAt(1, 0, 255, 0);
-		led.show();
-		delay(800);
-		led.setColorAt(0, 0, 0, 0);
-		led.setColorAt(1, 0, 0, 0);
-		led.show();
-		buzzer.tone(1000, 200);
-	}
-
-	led.setColorAt(0, 0, 0, 255);
-	led.setColorAt(1, 0, 0, 255);
-	led.show();
+  led.setColorAt(0, 0, 0, 255);
+  led.setColorAt(1, 0, 0, 255);
+  led.show();
 }
 
 
 void followTheLine()
 {
-	if (runSpeed < 255) {
-		if (speedupQuotient % 64 == 0){
-			runSpeed++;
-			speedupQuotient = 0;
-		}
-		else {
-			speedupQuotient++;
-		}
-	}
+  if (runSpeed < 255) {
+    if (speedupQuotient % 64 == 0) {
+      runSpeed++;
+      speedupQuotient = 0;
+    }
+    else {
+      speedupQuotient++;
+    }
+  }
 
-	uint8_t val = lineFinder.readSensors();
-	switch (val) {
+  uint8_t val = lineFinder.readSensors();
+  switch (val) {
 
-	case S1_IN_S2_IN:
-		forward();
-		lineFollowFlag = 10;
-		break;
+    case S1_IN_S2_IN:
+      forward();
+      lineFollowFlag = 10;
+      break;
 
-	case S1_IN_S2_OUT:
-		forward();
-		if (lineFollowFlag > 1) lineFollowFlag--;
-		break;
+    case S1_IN_S2_OUT:
+      forward();
+      if (lineFollowFlag > 1) lineFollowFlag--;
+      break;
 
-	case S1_OUT_S2_IN:
-		forward();
-		if (lineFollowFlag < 20) lineFollowFlag++;
-		break;
+    case S1_OUT_S2_IN:
+      forward();
+      if (lineFollowFlag < 20) lineFollowFlag++;
+      break;
 
-	case S1_OUT_S2_OUT:
-		if (fuzzyCounter < fuzzyAmount) {
-			fuzzyCounter++;
-			return;
-		} 
+    case S1_OUT_S2_OUT:
+      if (fuzzyCounter < fuzzyAmount) {
+        fuzzyCounter++;
+        return;
+      }
 
-		fuzzyCounter = 0;
-		runSpeed = minFallbackSpeed;
+      fuzzyCounter = 0;
+      runSpeed = minFallbackSpeed;
 
-		if (lineFollowFlag == 10) backward();
-		if (lineFollowFlag < 10) left();
-		if (lineFollowFlag > 10) right();
-		break;
-	}
+      if (lineFollowFlag == 10) backward();
+      if (lineFollowFlag < 10) left();
+      if (lineFollowFlag > 10) right();
+      break;
+  }
 }
 
 void evadeleft() {
